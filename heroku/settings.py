5661300b3115060ce0918dbc606e7381d6423ec9
gusdnr9875 <38147253+gusdnr9875@users.noreply.github.com>
2019-05-23 20:39:01 +0900

    second commit

--- heroku/settings.py
+++ heroku/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
