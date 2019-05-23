1ba33a0af4f5196be794583b06b73a9524b1e055
gusdnr9875 <38147253+gusdnr9875@users.noreply.github.com>
2019-05-23 21:38:14 +0900

    collectstatic

--- heroku/settings.py
+++ heroku/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -122,7 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
